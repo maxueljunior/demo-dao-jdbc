8d48ea4ede0804729d2c7dd30d783e8b112f610f
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-05 21:11:13 -0300

    Criado classe departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,40 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegreityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		PreparedStatement st = null;
+		Department obj = new Department(1, "Books");
 		
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
-			
-			st.setInt(1,2);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! " + rowsAffected);
-		}
-		catch(SQLException e) {
-			throw new DbIntegreityException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		System.out.println(obj);
 		
 	}
 

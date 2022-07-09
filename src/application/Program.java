eb7654dcbeb2a626c124ef8eb52a9afee740c0db
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-09 13:14:28 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -48,6 +51,17 @@ public class Program {
 		sellerDao.update(seller);
 		
 		System.out.println("Update ");
+		
+		System.out.println("\n=== TESTE 6: seller delete  ===");
+		
+		System.out.println("Entre com o ID do usuario");
+		int id = sc.nextInt();
+		
+		sellerDao.deleteById(id);
+		
+		System.out.println("Deletado");
+		
+		sc.close();
 		
 	}
 

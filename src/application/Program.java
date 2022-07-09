e0182d9f017a05aba94c5f50d0cc0211cb15377f
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-09 12:52:48 -0300

    FindAll implementacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,13 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 3: seller find all ===");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
